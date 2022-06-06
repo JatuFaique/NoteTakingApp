7605c78c9e35190a25b4a65fb110f95642bdf1a7
JatuFaique <jatu631@gmail.com>
2022-06-06 14:29:05 +0530

    added authreducer and logic

--- src/Context/Authcontext.js
+++ src/Context/Authcontext.js
@@ -1,68 +1,14 @@
-import axios from "axios";
-import { createContext, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext();
+import authReducer, { initialState } from "../reducers/authReducer";
+export const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
-  const axios = require("axios");
-  const navigate = useNavigate();
-
-  const [isLogin, setIsLogin] = useState(false);
-  const user = localStorage.getItem("user");
-  const token = localStorage.getItem("token");
-
-  const handleSingupData = async (email, password) => {
-    const getValues = { email, password };
-    try {
-      const res = await axios.post("/api/auth/signup", getValues);
-      if (res.status == 200 || res.status == 201) {
-        setIsLogin(true);
-        localStorage.setItem("token", res.data.encodedToken);
-        navigate("/login");
-      }
-    } catch (error) {
-      if (error.response.status == 422) {
-        alert("Successfully Registered, Please sign in");
-      }
-    }
-  };
-  const handleLoginData = async (email, password) => {
-    const getLoginValues = { email, password };
-    console.log(getLoginValues.email, getLoginValues.password);
-    try {
-      const res = await axios.post("/api/auth/login", {
-        email: getLoginValues.email,
-        password: getLoginValues.password,
-      });
-      console.log("ii", res.data);
-      if (res.status == 200 || res.status == 201) {
-        setIsLogin(true);
-
-        localStorage.setItem("token", res.data.encodedToken);
-        navigate("/home");
-        localStorage.setItem("user", res.data.foundUser.firstName);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+export const AuthProvider = ({ children }) => {
   return (
-    <AuthContext.Provider
-      value={{
-        isLogin,
-        setIsLogin,
-        handleSingupData,
-        handleLoginData,
-        user,
-        token,
-      }}
-    >
+    <AuthContext.Provider value={useReducer(authReducer, initialState)}>
       {children}
     </AuthContext.Provider>
   );
 };
-const useAuth = () => useContext(AuthContext);
 
-export { useAuth, AuthProvider };
+export const useAuth = () => useContext(AuthContext);
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,29 +1,89 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { useAuth } from "../Context/Authcontext";
+import axios from "axios";
 
 function Login() {
-  const { handleLoginData } = useAuth();
-
-  const [login, setLogin] = useState({
+  const [authState, authDispatch] = useAuth();
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [isError, setIsError] = useState("");
+  const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
-  const [isError, setIsError] = useState("");
+
+  const signUpToggle = () => {
+    setShowSignUp(!showSignUp);
+    console.log(showSignUp);
+  };
+
+  const handleSingupData = async (firstname, lastname, email, password) => {
+    const getValues = { firstname, lastname, email, password };
+    try {
+      authDispatch({
+        type: "REQUEST_SIGNUP",
+      });
+      const res = await axios.post("/api/auth/signup", getValues);
+      if (res.status == 200 || res.status == 201) {
+        localStorage.setItem("token", res.data.encodedToken);
+        const token = localStorage.getItem("token");
+        // console.log(res.data);
+        const email = res.data.createdUser.email;
+        const userInfo = { email, token };
+        authDispatch({
+          type: "SIGNUP_SUCCESS",
+          payload: userInfo,
+        });
+        console.log("uiui", userInfo);
+        // navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLoginData = async (email, password) => {
+    const getLoginValues = { email, password };
+    console.log(getLoginValues.email, getLoginValues.password);
+    try {
+      const res = await axios.post("/api/auth/login", {
+        email: getLoginValues.email,
+        password: getLoginValues.password,
+      });
+      console.log("ii", res.data);
+      if (res.status == 200 || res.status == 201) {
+        localStorage.setItem("token", res.data.encodedToken);
+        const token = localStorage.getItem("token");
+        const userInfo = { email, token };
+        authDispatch({
+          type: "SUCCESS_LOGIN",
+          payload: userInfo,
+        });
+        // navigate("/notes");
+      }
+    } catch (error) {
+      // console.log(error.response);
+      setIsError(error.response.data.errors);
+    }
+  };
+
   const handleData = (e) => {
-    setLogin({ ...login, [e.target.name]: e.target.value });
+    e.preventDefault();
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
   };
 
   const loginHandler = (e) => {
-    console.log("hell");
-    const { email, password } = login;
     e.preventDefault();
-    if (email !== "" && password !== "") {
-      handleLoginData(email, password);
-    } else {
-      setIsError("Please fill all the field");
-    }
-    setIsError("");
+
+    const { email, password } = loginData;
+    handleLoginData(email, password);
+  };
+
+  const signUpHandler = (e) => {
+    e.preventDefault();
+
+    const { firstname, lastname, email, password } = loginData;
+    handleSingupData(firstname, lastname, email, password);
   };
 
   const test = {
@@ -31,9 +91,10 @@ function Login() {
     password: "adarshBalika123",
   };
 
-  const useDemoValues = () => {
-    setLogin({
-      ...login,
+  const useDemoValues = (e) => {
+    e.preventDefault();
+    setLoginData({
+      ...loginData,
       email: test.email,
       password: test.password,
     });
@@ -43,54 +104,158 @@ function Login() {
     <div class="login__container logincomponent">
       <div class="wrapper">
         <div class="left__wrap">
-          <main class="auth-container">
-            <div class="auth-box p-1">
-              <p>Please Sign in</p>
-              <form class="userForm">
-                <section class="input-box">
-                  <div class="input-field">
-                    <input
-                      name="email"
-                      value={login.email}
-                      id="email-field"
-                      type="text"
-                      pattern=".*\S.*"
-                      onChange={handleData}
-                      required
-                    />
-                    <label for="email-field" class="placeholder">
-                      Enter Email
-                    </label>
-                  </div>
-                </section>
-                <section>
-                  <div class="input-field">
-                    <input
-                      id="email-field"
-                      name="password"
-                      type="text"
-                      pattern=".*\S.*"
-                      value={login.password}
-                      required
-                      onChange={handleData}
-                    />
-                    <label for="password-field" class="placeholder">
-                      Password
-                    </label>
-                  </div>
-                </section>
-                <button class="btn-prim" type="submit" onClick={useDemoValues}>
-                  User Demo Cred
-                </button>
-                <button class="btn-prim" type="submit" onClick={loginHandler}>
-                  Sign In
-                </button>
-              </form>
-              <button class="btn-secd" onClick="{signUpToggle}">
-                Create a New Account
-              </button>
-            </div>
-          </main>
+          {showSignUp ? (
+            <>
+              <main class="auth-container">
+                <div class="auth-box p-1">
+                  <p>{String(isError)}</p>
+                  <p>Please Sign in</p>
+
+                  <form class="userForm">
+                    <section class="input-box">
+                      <div class="input-field">
+                        <input
+                          name="firstname"
+                          value={loginData.firstname}
+                          id="email-field"
+                          type="text"
+                          pattern=".*\S.*"
+                          onChange={handleData}
+                          required
+                        />
+                        <label for="email-field" class="placeholder">
+                          Enter firstname
+                        </label>
+                      </div>
+                    </section>
+                    <section class="input-box">
+                      <div class="input-field">
+                        <input
+                          name="lastname"
+                          value={loginData.lastname}
+                          id="email-field"
+                          type="text"
+                          pattern=".*\S.*"
+                          onChange={handleData}
+                          required
+                        />
+                        <label for="email-field" class="placeholder">
+                          Enter lastname
+                        </label>
+                      </div>
+                    </section>
+                    <section class="input-box">
+                      <div class="input-field">
+                        <input
+                          name="email"
+                          value={loginData.email}
+                          id="email-field"
+                          type="text"
+                          pattern=".*\S.*"
+                          onChange={handleData}
+                          required
+                        />
+                        <label for="email-field" class="placeholder">
+                          Enter Email
+                        </label>
+                      </div>
+                    </section>
+                    <section>
+                      <div class="input-field">
+                        <input
+                          id="email-field"
+                          name="password"
+                          type="text"
+                          pattern=".*\S.*"
+                          value={loginData.password}
+                          required
+                          onChange={handleData}
+                        />
+                        <label for="password-field" class="placeholder">
+                          Password
+                        </label>
+                      </div>
+                    </section>
+
+                    <button
+                      class="btn-prim"
+                      type="submit"
+                      onClick={signUpHandler}
+                    >
+                      Sign Up
+                    </button>
+                  </form>
+                  <button class="btn-secd" onClick={signUpToggle}>
+                    Already Have Account
+                  </button>
+                </div>
+              </main>
+            </>
+          ) : (
+            <>
+              <main class="auth-container">
+                <div class="auth-box p-1">
+                  <p>{String(isError)}</p>
+                  <p>Please Sign in</p>
+
+                  <form class="userForm">
+                    <section class="input-box">
+                      <div class="input-field">
+                        <input
+                          name="email"
+                          value={loginData.email}
+                          id="email-field"
+                          type="text"
+                          pattern=".*\S.*"
+                          onChange={handleData}
+                          required
+                        />
+                        <label for="email-field" class="placeholder">
+                          Enter Email
+                        </label>
+                      </div>
+                    </section>
+                    <section>
+                      <div class="input-field">
+                        <input
+                          id="email-field"
+                          name="password"
+                          type="text"
+                          pattern=".*\S.*"
+                          value={loginData.password}
+                          required
+                          onChange={handleData}
+                        />
+                        <label for="password-field" class="placeholder">
+                          Password
+                        </label>
+                      </div>
+                    </section>
+
+                    <button
+                      class="btn-prim"
+                      type="submit"
+                      onClick={loginHandler}
+                    >
+                      Sign In
+                    </button>
+                  </form>
+                  <button class="btn-secd" onClick={signUpToggle}>
+                    Create a New Account
+                  </button>
+                  <span>
+                    <button
+                      class="btn-prim"
+                      type="submit"
+                      onClick={useDemoValues}
+                    >
+                      User Demo Cred
+                    </button>
+                  </span>
+                </div>
+              </main>
+            </>
+          )}
         </div>
         <div class="right__wrap">
           <h1>Get Your Important Stuff Done!</h1>
